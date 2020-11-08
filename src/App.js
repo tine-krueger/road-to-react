7c682af5b6862fcf8b07ae2753c49169239bd799
tine-krueger <krueger.christ@gmail.com>
2020-11-08 12:13:54 +0100

    First Component

--- src/App.js
+++ src/App.js
@@ -36,18 +36,24 @@ function App() {
       <label htmlFor="search"> Search: </label>
       <input id="search" type="text"/>
       <hr />
-      {list.map(function(item) {
-        return <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          </div>
-      })}
+      <List/>
+      
     </div>
   );
 }
 
 export default App;
+
+function List() {
+  return list.map(function(item) {
+    return (<div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      </div>
+    )
+  })
+}

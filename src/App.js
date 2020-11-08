f0373be14266ea7f4185252ba67995afe98a2334
tine-krueger <krueger.christ@gmail.com>
2020-11-08 16:06:58 +0100

    Callback functions

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import './App.css'
-
-const list1 = [
-  {
-    title: 'React',
-    url: 'http://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'http://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-]
+import {useState} from 'react'
 
 
 const App = () => {
-  //do something
-  const handleChange = event => {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'http://reactjs.org',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: 'Redux',
+      url: 'http://redux.js.org',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ]
+
+  function handleSearch(event) {
     console.log(event.target.value)
   }
+
+  
 
   return (
     <div className="App">
       <h1>My Hacker Stories</h1>
-      
-
-      <label htmlFor="search"> Search: </label>
-      <input id="search" type="text" onChange={handleChange}/>
+      <Search onSearch={handleSearch}/>
       <hr />
-      <List list={list1}/>
+      <List list={stories}/>
       
     </div>
   );
@@ -42,8 +42,24 @@ const App = () => {
 
 export default App;
 
-const List = ({list}) => {
-  return list.map(item => 
+const Search = props => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleChange = event => {
+    props.onSearch(event)
+    setSearchTerm(event.target.value)
+  }
+
+  return (
+      <div>
+        <label htmlFor="search"> Search: </label>
+        <input id="search" type="text" onChange={handleChange}/>
+
+        <p>Searching for: <strong>{searchTerm}</strong></p>
+      </div>)
+}
+ 
+const List = (props) => props.list.map(item => 
      (<div key={item.objectID}>
       <span>
         <a href={item.url}>{item.title}</a>
@@ -54,4 +70,4 @@ const List = ({list}) => {
       </div>
     )
   )
-}
+

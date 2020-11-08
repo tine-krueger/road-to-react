1859e0f589de05cb6251256e1332350cdcd20135
tine-krueger <krueger.christ@gmail.com>
2020-11-08 12:06:49 +0100

    handle Arrays

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ function getTitle(title) {
   return title
 }
 
-const number = 1
-const nully = null
-const undefine = undefined
-const booly = true
-const list = ['Heyy', 'there']
+
+const list = [
+  {
+    title: 'React',
+    url: 'http://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'http://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+]
 
 function App() {
 
@@ -16,15 +30,22 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello {getTitle('React')}</h1>
-      <p>{number}</p>
-      <p>{nully}</p>
-      <p>{undefine}</p>
-      <p>{console.log(booly)}</p>
-      {list.map(element => <p>{element}</p>)}
+      <h1>My Hacker Stories</h1>
+      
 
       <label htmlFor="search"> Search: </label>
       <input id="search" type="text"/>
+      <hr />
+      {list.map(function(item) {
+        return <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          </div>
+      })}
     </div>
   );
 }
